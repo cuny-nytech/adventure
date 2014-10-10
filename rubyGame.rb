874798395e3d459b09e081bcd36d62c3b5a3bbb3
Hector Pacheco <jozcar@yahoo.com>
2014-10-10 17:47:16 -0400

    Update rubyGame.rb

--- rubyGame.rb
+++ rubyGame.rb
@@ -1,35 +1,34 @@
-def printStories(chapter)
+def print_stories(chapter)
   chapter + 1
 end
 
-def printStoriesText(chapter)
-   (0...50).map { ('a'..'z').to_a[rand(chapter.to_i)] }.join
+def print_stories_text(chapter)
+  (0...50).map { ('a'..'z').to_a[rand(chapter.to_i)] }.join
 end
 
-def getChapters(index)
+def get_chapters(index)
   "Chapter #{index}"
 end
 
-def chapterSelection(chapter)
+def chapter_selection(chapter)
   puts('You have selected ')
-  puts(getChapters(chapter))
+  puts(get_chapters(chapter))
   print("Here is your Story \n")
-  puts printStoriesText(chapter.to_i)
+  puts print_stories_text(chapter.to_i)
 end
 
 print('Please Select Chapter stories use numbers to indicate chapter number: ')
 chapter = gets
-chapterSelection(chapter)
+chapter_selection(chapter)
 continue = true
-
 
 while continue ==  true
   print('Select Chapter stories use numbers to continue to the next story: ')
   chapter = gets
-  chapterSelection(chapter)
+  chapter_selection(chapter)
   print('To continue, please type  {1, for continue}{0, for cancel}')
   go = gets
-  if(go.to_i == 0)
+  if go.to_i == 0
     continue = false
   end
 end
